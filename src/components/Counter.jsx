e70755870cce4e09b8d821af242cc30fb87cc1fa
Cameron Romo <cromo1225@gmail.com>
2023-01-21 11:57:24 -0700

    Add counter component to dynamically render totals

---
+++ src/components/Counter.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  render() {
+    return (
+      <>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </>
+    );
+  }
+}
+
+export default Counter;
