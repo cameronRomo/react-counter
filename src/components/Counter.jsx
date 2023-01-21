c96803678c133aebf6a648fe9dbe98233b0bdff9
Cameron Romo <cromo1225@gmail.com>
2023-01-21 12:37:12 -0700

    Add increment function

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,24 +3,41 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
   };
 
-  formatCount() {
+  handleIncrement = () => {
+    this.setState({
+      count: ++this.state.count,
+    });
+  };
+
+  formatCount = () => {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
-  }
+  };
 
-  getBadgeClasses() {
+  getBadgeClasses = () => {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
-  }
+  };
 
   render() {
     return (
       <>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          className="btn btn-secondary btn-sm"
+          onClick={this.handleIncrement}
+        >
+          Increment
+        </button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </>
     );
   }
