bd0abc4d4d2ede66d3778a5f9e8784bfb0ee41e0
Cameron Romo <cromo1225@gmail.com>
2023-01-23 12:06:07 -0700

    Add event handler for liking a movie, and render like icon

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Like from "../common/Like";
 
 class Movies extends Component {
   state = {
@@ -15,13 +16,24 @@ class Movies extends Component {
   handleDelete = (movie) => {
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
     this.setState({
-      movies
+      movies,
+    });
+  };
+
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({
+      movies,
     });
   };
 
   render() {
     const { length: count } = this.state.movies;
-    if (this.state.movies.length === 0) return <p>There are no movies in the database.</p>
+    if (this.state.movies.length === 0)
+      return <p>There are no movies in the database.</p>;
 
     return (
       <>
@@ -34,6 +46,7 @@ class Movies extends Component {
               <th>Stock</th>
               <th>Rate</th>
               <th></th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -43,6 +56,12 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onLikeToggle={() => this.handleLike(movie)}
+                  />
+                </td>
                 <td>
                   <button
                     className="btn btn-danger btn-sm"
