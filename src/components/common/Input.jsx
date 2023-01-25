62ecc2eb0004bf040901a8dc5f7ec97d9b53f705
Cameron Romo <cromo1225@gmail.com>
2023-01-25 11:17:27 -0700

    Add rest and spread operators to spread in additional properties

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -4,7 +4,12 @@ const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input {...rest} id={name} name={name} className="from-control" />
+      <input
+        {...rest}
+        id={name}
+        name={name}
+        className="from-control row ml-0"
+      />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
