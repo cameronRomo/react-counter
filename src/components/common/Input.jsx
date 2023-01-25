96bab634a7022b3599980066829c2c231b0c0f84
Cameron Romo <cromo1225@gmail.com>
2023-01-25 09:32:04 -0700

    Fix mistake

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -3,12 +3,10 @@ import React from "react";
 const Input = ({ name, label, value, onChange }) => {
   return (
     <div className="form-group">
-      <label htmlFor="name">{ label }</label>
+      <label htmlFor={name}>{label}</label>
       <input
-        autoFocus
-        ref={name}
-        id="name"
-        name="name"
+        id={name}
+        name={name}
         type="text"
         value={value}
         onChange={onChange}
