d06f04998040c07eaeae9b136ff5dde518800ec5
Cameron Romo <cromo1225@gmail.com>
2023-02-01 15:03:36 -0700

    Webpack not importing from config.json workaround

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,9 +1,17 @@
 import http from "./httpService";
-import { apiUrl } from "../config.json";
+// import { apiUrl } from "../config.json";
 
-const API_ENDPOINT = apiUrl + "/movies";
+const API_ENDPOINT = "http://localhost:3900/api" + "/movies";
 export function getMovies() {
   return http.get(API_ENDPOINT);
+}
+
+export function getMovie(movieId) {
+  return http.get(API_ENDPOINT + "/" + movieId);
+}
+
+export function saveMovie(movieId) {
+  return http.get(API_ENDPOINT + "/" + movieId);
 }
 
 export function deleteMovie(movieId) {
